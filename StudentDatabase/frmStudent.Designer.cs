9f32ef395f74b8971dff4a3e63f2ecfc445e46fb
Cody <54541144+CodyDickson@users.noreply.github.com>
2019-09-25 19:57:50 -0400

    Add files via upload

---
+++ StudentDatabase/frmStudent.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace StudentDatabase
+{
+    partial class frmStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSSN = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtSSN = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSSN
+            // 
+            this.lblSSN.AutoSize = true;
+            this.lblSSN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSSN.Location = new System.Drawing.Point(25, 22);
+            this.lblSSN.Name = "lblSSN";
+            this.lblSSN.Size = new System.Drawing.Size(39, 16);
+            this.lblSSN.TabIndex = 0;
+            this.lblSSN.Text = "SSN:";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(25, 58);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(76, 16);
+            this.lblLastName.TabIndex = 1;
+            this.lblLastName.Text = "Last Name:";
+            // 
+            // txtSSN
+            // 
+            this.txtSSN.Location = new System.Drawing.Point(120, 22);
+            this.txtSSN.Name = "txtSSN";
+            this.txtSSN.Size = new System.Drawing.Size(121, 20);
+            this.txtSSN.TabIndex = 2;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(120, 57);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(121, 20);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFind.Location = new System.Drawing.Point(269, 22);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 4;
+            this.btnFind.Text = "&Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.BtnFind_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(28, 105);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(28, 144);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModify.Location = new System.Drawing.Point(145, 105);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(75, 23);
+            this.btnModify.TabIndex = 7;
+            this.btnModify.Text = "&Modify";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.BtnModify_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.Location = new System.Drawing.Point(145, 144);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 8;
+            this.btnShow.Text = "&Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.BtnShow_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(269, 105);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "&Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(269, 144);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // frmStudent
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(367, 192);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtSSN);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblSSN);
+            this.Name = "frmStudent";
+            this.Text = "Student Database";
+            this.Load += new System.EventHandler(this.FrmStudent_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSSN;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtSSN;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ StudentDatabase/frmStudent.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentDatabase
+{
+    public partial class frmStudent : Form
+    {
+        // Cody Dickson
+        // M4 Assignment
+        // CPT 231-741
+        // Fall 2019
+
+        public frmStudent()
+        {
+            InitializeComponent();
+        }
+
+        // Close the form
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Reset the form to initial state
+        private void Reset()
+        {
+            txtSSN.ReadOnly = false;
+            btnFind.Enabled = true;
+            btnAdd.Enabled = true;
+            btnModify.Enabled = false;
+            btnDelete.Enabled = false;
+            txtSSN.Text = "";
+            txtLastName.Text = "";
+        }
+
+        // On Form Load, call the Reset method
+        private void FrmStudent_Load(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        // Class SortedList variable declaration
+        SortedList<int, string> studentDatabase = new SortedList<int, string>();
+
+        // Add Button - validates the form with the IsValidData method, then adds the data to studentDatabase (SortedList)
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsValidData())
+                {
+                    studentDatabase.Add(Int32.Parse(txtSSN.Text), txtLastName.Text);
+                    Reset();
+                }
+            }
+            catch (Exception ex)
+            {
+                // General Exception
+                MessageBox.Show(ex.Message + "\n\n" + ex.GetType().ToString() + "\n" + ex.StackTrace, "General Exception");
+            }
+        }
+
+        // Verifies that the SSN is 9 digits
+        private bool IsValidSSN(string SSN)
+        {
+            int number;
+            if (SSN.Length == 9 & int.TryParse(SSN, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("SSN must include 9 valid numeric numbers.", "Entry Error");
+                return false;
+            }
+        }
+
+        // Verifies that the LastName is present
+        private bool IsValidLastName(string lastName)
+        {
+            if (txtLastName.Text == "")
+            {
+                MessageBox.Show("Last Name field cannot be blank.", "Entry Error");
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+
+        // Form validation method
+        private bool IsValidData()
+        {
+            if (!IsValidSSN(txtSSN.Text)) return false;
+            if (!IsValidLastName(txtLastName.Text)) return false;
+
+            return true;
+        }
+
+        // Gets the SSN from the textbox and finds it in the SortedList
+        // Tests the value of the SSN by searching studentDatabase (the Sorted List) for the key
+        // If the key is present, it pulls the value that matches it and assigns it to the Last Name text box
+        private void BtnFind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsValidSSN(txtSSN.Text))
+                {
+                    int value = Int32.Parse(txtSSN.Text);
+                    if (studentDatabase.ContainsKey(value))
+                    {
+                        txtLastName.Text = Convert.ToString(studentDatabase[value]);
+                        btnModify.Enabled = true;
+                        btnDelete.Enabled = true;
+                        btnAdd.Enabled = false;
+                        txtSSN.ReadOnly = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Student could not be found.", "Student Not Found");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // General Exception
+                MessageBox.Show(ex.Message + "\n\n" + ex.GetType().ToString() + "\n" + ex.StackTrace, "General Exception");
+            }
+        }
+
+        // Takes the value in the SSN text box, finds that Key in the Sorted List, and updates the Last Name value
+        private void BtnModify_Click(object sender, EventArgs e)
+        {
+            int value = Int32.Parse(txtSSN.Text);
+            studentDatabase[value] = txtLastName.Text;
+            MessageBox.Show("Student was updated in the database.", "Student Updated");
+        }
+
+        // Removes an entry from the Sorted List using the SSN Text Box as the key
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            int value = Int32.Parse(txtSSN.Text);
+            studentDatabase.Remove(value);
+            MessageBox.Show("Student removed from the database.", "Student Removed");
+            Reset();
+        }
+
+        // Resets the form
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        // Cycles through the Sorted List, storing its Keys and Values in a string, and then displays that string in a Message Box
+        private void BtnShow_Click(object sender, EventArgs e)
+        {
+            string results = "";
+            foreach (KeyValuePair<int, string> student in studentDatabase)
+            {
+                results += student.Key + "\t" + student.Value + "\n";
+            }
+            MessageBox.Show(results, "Student List");
+        }
+    }
+}
